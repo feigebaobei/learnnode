a7d76a2abc74a16f91f20754d769ff412428f9c1
feigebaobei <18515195415@163.com>
2018-10-15 09:16:05 +0800

    basis

---
+++ index.js
@@ -0,0 +1,8 @@
+let http = require('http'),
+  url = require('url')
+function start (route) {
+  http.createServer(function (request, response) {
+    
+  }.listen(8888))
+  console.log('server has started')
+}
--- my-web-server.js
+++ my-web-server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+// var eventEmitter = new events.EventEmitter();
 var serv = http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('<marquee>Smashing Node!</marquee>');
