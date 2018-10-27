affe5a92bdac285aa893f0d88df7e51141e2bd43
feigebaobei <18515195415@163.com>
2018-10-27 21:33:33 +0800

    init sbject

---
+++ server.js
@@ -0,0 +1,6 @@
+const http = require('http')
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end('hello world\n')
+}).listen(8888)
+console.log('http://127.0.0.1:8888/')
