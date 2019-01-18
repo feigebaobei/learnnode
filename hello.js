9cde8ad6b84b72cd2b0ea8b5cbd8b526bf7865fa
feigebaobei <18515195415@163.com>
2019-01-18 17:43:54 +0800

    del all

--- hello.js
+++
@@ -1,3 +0,0 @@
-console.log('hello world')
-console.error('hello error')
-console.log('%s %d', 'str', 22)
--- readFileSync.js
+++
@@ -1,4 +0,0 @@
-let fs = require('fs')
-var data = fs.readFileSync('file.txt', 'utf-8');
-console.log(data)
-console.log('end')
