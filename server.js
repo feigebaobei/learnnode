8d8ed52b4ff8d4182dfb86daabd28904d292c4b3
feigebaobei <18515195415@163.com>
2019-03-14 17:25:04 +0800

    在首页使用活数据

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express')
 var config = require('./server/configure.js')
 var app = express()
+var mongoose = require('mongoose')
 app.set('port', process.env.PORT || 3800)
 app.set('views', __dirname + '/views')
 app = config(app)
 // app.get('/', function (req, res) {
 //   res.send('hello world')
 // })
-app.listen(app.get('port'), function () {
-  console.log(`server up: http://localhost:${app.get('port')}`)
+mongoose.connect('mongodb://localhost:27017/imgPloadr', {useNewUrlParser: true})
+mongoose.connection.on('open', () => {
+  console.log('mongoose connected.')
 })
+app.listen(app.get('port'), function () {
+  console.log(`server up: http://localhost:${app.get('port')}`)})
